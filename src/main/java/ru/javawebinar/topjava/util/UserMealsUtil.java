ade783755e3be4c4b31d36583fe1888c3e0da7e2
nkuznetsov <nkuznetsov87@mail.ru>
2015-06-08 17:07:02 +0300

    hw0 fixes

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -3,13 +3,17 @@ package ru.javawebinar.topjava.util;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.model.UserMealWithExceed;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import static java.util.stream.Collectors.*;
 import static ru.javawebinar.topjava.util.TimeUtil.isBetween;
 
 /**
@@ -22,9 +26,9 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,10,0), "Завтрак", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,13,0), "Обед", 500),
-                new UserMeal(LocalDateTime.of(2015, Month.MAY, 30,20,0), "Ужин", 500)
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,10,0), "Завтрак", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,13,0), "Обед", 500),
+                new UserMeal(LocalDateTime.of(2015, Month.MAY, 31,20,0), "Ужин", 500)
         );
         getFilteredMealsWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12,0), 2000);
 //        .toLocalDate();
@@ -32,10 +36,25 @@ public class UserMealsUtil {
     }
 
     public static List<UserMealWithExceed> getFilteredMealsWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int maxCaloriesPerDay) {
-        return mealList.stream()
-                .filter(m -> m.getCalories() > maxCaloriesPerDay)
-                .filter(m -> isBetween(m.getDateTime().toLocalTime(), startTime, endTime))
-                .map(m -> new UserMealWithExceed(m.getDateTime(), m.getDescription(), m.getCalories(), true))
-                .collect(Collectors.toList());
+        List<UserMeal> mealsFilteredByDate = mealList.stream()
+                .filter(um -> isBetween(um.getDateTime().toLocalTime(), startTime, endTime))
+                .collect(toList());
+
+        Map<LocalDate, Integer> dateToCalories = mealsFilteredByDate.stream()
+                .collect(groupingBy(um -> um.getDateTime().toLocalDate(),
+                        reducing(0,
+                                UserMeal::getCalories,
+                                Integer::sum
+                        )
+                ));
+
+        List<UserMealWithExceed> result = mealsFilteredByDate.stream()
+                .map(um -> new UserMealWithExceed(um.getDateTime(),
+                        um.getDescription(),
+                        um.getCalories(),
+                        dateToCalories.get(um.getDateTime().toLocalDate()) > maxCaloriesPerDay))
+                .collect(toList());
+
+        return result;
     }
 }
--- src/test/java/ru/javawebinar/topjava/util/UserMealsUtilTest.java
+++ src/test/java/ru/javawebinar/topjava/util/UserMealsUtilTest.java
@@ -11,67 +11,62 @@ import java.time.Month;
 import java.util.Arrays;
 import java.util.List;
 
+import static java.util.Arrays.asList;
 import static junit.framework.Assert.assertEquals;
 
 public class UserMealsUtilTest {
-    private final static List<UserMeal> MEAL_LIST = Arrays.asList(
+    private final static List<UserMeal> MEAL_LIST = asList(
             new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 200),
             new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 300),
             new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 400),
-            new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 10, 0), "Завтрак", 500),
-            new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 13, 0), "Обед", 600),
-            new UserMeal(LocalDateTime.of(2015, Month.MAY, 30, 20, 0), "Ужин", 700)
+            new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 10, 0), "Завтрак", 500),
+            new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 600),
+            new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 700)
     );
 
     @Test
     public void testAllExceed() throws Exception {
         List<UserMealWithExceed> filtered = UserMealsUtil.getFilteredMealsWithExceeded(MEAL_LIST, LocalTime.of(10, 0), LocalTime.of(20, 0), 150);
         assertFiltered(filtered, 6);
+        assertExceedFlags(filtered, asList(true, true, true, true, true, true));
     }
 
     @Test
     public void testNoneExceed() throws Exception {
-        List<UserMealWithExceed> filtered = UserMealsUtil.getFilteredMealsWithExceeded(MEAL_LIST, LocalTime.of(10, 0), LocalTime.of(20, 0), 700);
-        assertFiltered(filtered, 0);
-    }
-
-    @Test
-    public void testSingleDoesNotExceed() throws Exception {
-        List<UserMealWithExceed> filtered = UserMealsUtil.getFilteredMealsWithExceeded(MEAL_LIST, LocalTime.of(10, 0), LocalTime.of(20, 0), 200);
-        assertFiltered(filtered, 5);
+        List<UserMealWithExceed> filtered = UserMealsUtil.getFilteredMealsWithExceeded(MEAL_LIST, LocalTime.of(10, 0), LocalTime.of(20, 0), 2000);
+        assertFiltered(filtered, 6);
+        assertExceedFlags(filtered, asList(false, false, false, false, false, false));
     }
 
     @Test
     public void testOutOfStartTime() throws Exception {
-        List<UserMealWithExceed> filtered = UserMealsUtil.getFilteredMealsWithExceeded(MEAL_LIST, LocalTime.of(13, 0), LocalTime.of(20, 0), 150);
+        List<UserMealWithExceed> filtered = UserMealsUtil.getFilteredMealsWithExceeded(MEAL_LIST, LocalTime.of(13, 0), LocalTime.of(20, 0), 800);
         assertFiltered(filtered, 4);
+        assertExceedFlags(filtered, asList(false, false, true, true));
     }
 
     @Test
     public void testOutOfEndTime() throws Exception {
-        List<UserMealWithExceed> filtered = UserMealsUtil.getFilteredMealsWithExceeded(MEAL_LIST, LocalTime.of(10, 0), LocalTime.of(19, 59), 150);
+        List<UserMealWithExceed> filtered = UserMealsUtil.getFilteredMealsWithExceeded(MEAL_LIST, LocalTime.of(10, 0), LocalTime.of(19, 59), 400);
         assertFiltered(filtered, 4);
-    }
-
-    @Test
-    public void testAllAffect() throws Exception {
-        List<UserMealWithExceed> filtered = UserMealsUtil.getFilteredMealsWithExceeded(MEAL_LIST, LocalTime.of(11, 0), LocalTime.of(19, 59), 400);
-        assertFiltered(filtered, 1);
+        assertExceedFlags(filtered, asList(true, true, true, true));
     }
 
     @Test
     public void testNoneSelected() throws Exception {
-        List<UserMealWithExceed> filtered = UserMealsUtil.getFilteredMealsWithExceeded(MEAL_LIST, LocalTime.of(11, 0), LocalTime.of(19, 59), 800);
+        List<UserMealWithExceed> filtered = UserMealsUtil.getFilteredMealsWithExceeded(MEAL_LIST, LocalTime.of(13, 1), LocalTime.of(19, 59), 800);
         assertFiltered(filtered, 0);
     }
 
 
     private static void assertFiltered(List<UserMealWithExceed> filtered, int size) {
         assertEquals("Wrong number of exceeded entities", size, filtered.size());
+    }
 
-        //TODO: assert that (but now there are no getters)
-        //all flags are true
-        //date within range
-        //calories exceed limit
+    private static void assertExceedFlags(List<UserMealWithExceed> filtered, List<Boolean> flags) {
+        assertEquals("User meals and exceed flags have different size", filtered.size(), flags.size());
+        for (int i = 0; i < filtered.size(); i++) {
+            assertEquals("Wrong exceed flag at index " + i, flags.get(i).booleanValue(), filtered.get(i).isExceed());
+        }
     }
 }

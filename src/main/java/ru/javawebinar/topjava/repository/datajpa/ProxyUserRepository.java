2ffb554f4f1f5064dc2a5a64572670d0b5cb8f43
nkuznetsov <nkuznetsov87@mail.ru>
2015-08-10 19:37:36 +0300

    before hw5

--- src/main/java/ru/javawebinar/topjava/repository/datajpa/ProxyUserRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/datajpa/ProxyUserRepository.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.repository.datajpa;
 
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
@@ -17,6 +18,7 @@ public interface ProxyUserRepository extends JpaRepository<User, Integer> {
 
     @Transactional
 //    @Query(name = User.DELETE)
+    @Modifying
     @Query("DELETE FROM User u WHERE u.id=:id")
     int delete(@Param("id") int id);
 

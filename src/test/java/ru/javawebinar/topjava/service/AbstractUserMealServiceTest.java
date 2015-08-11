cef2f8fb3bbadd0f34b6ba903b8bd5a8bebbc7fb
nkuznetsov <nkuznetsov87@mail.ru>
2015-08-11 20:11:57 +0300

    hw5 done

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserMealServiceTest.java
@@ -4,13 +4,8 @@ import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.jdbc.Sql;
-import org.springframework.test.context.jdbc.SqlConfig;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import ru.javawebinar.topjava.MealTestData;
 import ru.javawebinar.topjava.model.UserMeal;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
@@ -23,15 +18,7 @@ import static ru.javawebinar.topjava.MealTestData.*;
 import static ru.javawebinar.topjava.UserTestData.ADMIN_ID;
 import static ru.javawebinar.topjava.UserTestData.USER_ID;
 
-@ContextConfiguration({
-        "classpath:spring/spring-app.xml",
-        "classpath:spring/spring-db.xml"
-})
-@RunWith(SpringJUnit4ClassRunner.class)
-@Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
-@ActiveProfiles("postgres")
-@Sql("classpath:db/populateDB.sql")
-public class UserMealServiceTest {
+abstract public class AbstractUserMealServiceTest extends AbstractServiceTest {
 
     @Rule
     public ExpectedException thrown = ExpectedException.none();
